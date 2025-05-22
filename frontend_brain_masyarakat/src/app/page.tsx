c7b5685d4ea467cf774019f7f9bc367494a12a20
jaheerman3312301105 <frdhn2323@gmail.com>
2025-05-22 09:28:43 +0700

    update card cara melapor

--- frontend_brain_masyarakat/src/app/page.tsx
+++ frontend_brain_masyarakat/src/app/page.tsx
@@ -1,9 +1,10 @@
+import CardCaraMelapor from "@/components/caramelapor/cardcaramelapor";
 import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <div className="w-full px-9 py-6 bg-white">
-      <Footer></Footer>
+      <CardCaraMelapor></CardCaraMelapor>
     </div>
   );
 }
---
+++ frontend_brain_masyarakat/src/components/caramelapor/cardcaramelapor.tsx
@@ -0,0 +1,28 @@
+const CardCaraMelapor = () => {
+    return (
+        <div className="w-full h-[6rem] md:h-[7rem] lg:h-[7.5rem] border-[#BFBFBF] border-2 rounded-3xl">
+            <div className="w-full h-full flex flex-row  place-items-center p-4">
+                <IconCara icon={
+                    <svg className="w-full h-full" xmlns="http://www.w3.org/2000/svg" fill="#0063F4" viewBox="0 0 256 256">
+                        <path d="M168,152a8,8,0,0,1-8,8H96a8,8,0,0,1,0-16h64A8,8,0,0,1,168,152Zm-8-40H96a8,8,0,0,0,0,16h64a8,8,0,0,0,0-16Zm56-64V216a16,16,0,0,1-16,16H56a16,16,0,0,1-16-16V48A16,16,0,0,1,56,32H92.26a47.92,47.92,0,0,1,71.48,0H200A16,16,0,0,1,216,48ZM96,64h64a32,32,0,0,0-64,0ZM200,48H173.25A47.93,47.93,0,0,1,176,64v8a8,8,0,0,1-8,8H88a8,8,0,0,1-8-8V64a47.93,47.93,0,0,1,2.75-16H56V216H200Z">
+                        </path>
+                    </svg>
+                }/>
+                <div className="pl-4 text-black">
+                    <div className="font-medium text-sm">Isi Form.</div>
+                    <div className="font-normal text-xs">Masukkan judul, jenis, dan deskripsi pengaduan</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const IconCara = ({icon}: any) => {
+    return (
+        <div className="bg-[#DBEAFE] w-16 rounded-full place-items-center place-content-center p-2 md:p-3.5">
+            {icon}
+        </div>
+    )
+}
+
+export default CardCaraMelapor;
