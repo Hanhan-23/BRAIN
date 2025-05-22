94e212486750489fe33f2c344392eb4d4be5f37c
jaheerman3312301105 <frdhn2323@gmail.com>
2025-05-22 09:40:28 +0700

    update card cara melapor with responsive

--- frontend_brain_masyarakat/src/app/page.tsx
+++ frontend_brain_masyarakat/src/app/page.tsx
@@ -1,10 +1,9 @@
-import CardCaraMelapor from "@/components/caramelapor/cardcaramelapor";
 import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <div className="w-full px-9 py-6 bg-white">
-      <CardCaraMelapor></CardCaraMelapor>
+      <Footer></Footer>
     </div>
   );
 }
--- frontend_brain_masyarakat/src/components/caramelapor/cardcaramelapor.tsx
+++ frontend_brain_masyarakat/src/components/caramelapor/cardcaramelapor.tsx
@@ -1,16 +1,19 @@
-const CardCaraMelapor = () => {
+interface CardCaraMelapor {
+    title: string;
+    description: string;
+    icons: any;
+}
+
+const CardCaraMelapor = ({title, description, icons}: CardCaraMelapor) => {
     return (
         <div className="w-full h-[6rem] md:h-[7rem] lg:h-[7.5rem] border-[#BFBFBF] border-2 rounded-3xl">
             <div className="w-full h-full flex flex-row  place-items-center p-4">
                 <IconCara icon={
-                    <svg className="w-full h-full" xmlns="http://www.w3.org/2000/svg" fill="#0063F4" viewBox="0 0 256 256">
-                        <path d="M168,152a8,8,0,0,1-8,8H96a8,8,0,0,1,0-16h64A8,8,0,0,1,168,152Zm-8-40H96a8,8,0,0,0,0,16h64a8,8,0,0,0,0-16Zm56-64V216a16,16,0,0,1-16,16H56a16,16,0,0,1-16-16V48A16,16,0,0,1,56,32H92.26a47.92,47.92,0,0,1,71.48,0H200A16,16,0,0,1,216,48ZM96,64h64a32,32,0,0,0-64,0ZM200,48H173.25A47.93,47.93,0,0,1,176,64v8a8,8,0,0,1-8,8H88a8,8,0,0,1-8-8V64a47.93,47.93,0,0,1,2.75-16H56V216H200Z">
-                        </path>
-                    </svg>
+                    [icons]
                 }/>
                 <div className="pl-4 text-black">
-                    <div className="font-medium text-sm">Isi Form.</div>
-                    <div className="font-normal text-xs">Masukkan judul, jenis, dan deskripsi pengaduan</div>
+                    <div className="font-medium text-sm">{title}</div>
+                    <div className="font-normal text-xs">{description}</div>
                 </div>
             </div>
         </div>
