176e670383afb7fb1597675f822c1548eae6933d
jaheerman3312301105 <frdhn2323@gmail.com>
2025-05-23 07:17:49 +0700

    update form lapor

--- frontend_brain_masyarakat/src/app/page.tsx
+++ frontend_brain_masyarakat/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Footer from "@/components/footer";
+import FormLapor from "@/components/lapor/formlapor";
 
 export default function Home() {
   return (
-    <div className="w-full px-9 py-6 bg-white">
-      <Footer></Footer>
+    <div className="w-full px-3 py-6 bg-white">
+      <FormLapor/>
     </div>
   );
 }
