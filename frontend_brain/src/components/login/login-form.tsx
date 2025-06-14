837a5a78443d7395be00992f3108c5bc2ec9f063
michael3312301112 <liong888g@gmail.com>
2025-06-14 13:58:10 +0700

    halaman login masyarakat dan entry login pemerintah

---
+++ frontend_brain/src/components/login/login-form.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+import { WarningCircleIcon } from "@phosphor-icons/react";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="py-16">
+        <CardHeader className="text-center">
+          <CardTitle className="text-3xl font-bold">Masuk ke Brain</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid gap-6">
+              <div className="flex flex-col gap-4">
+                <Button variant="outline" className="w-full rounded-full">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="256"
+                    height="262"
+                    viewBox="0 0 256 262"
+                  >
+                    <path
+                      fill="#4285f4"
+                      d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
+                    />
+                    <path
+                      fill="#34a853"
+                      d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"
+                    />
+                    <path
+                      fill="#fbbc05"
+                      d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z"
+                    />
+                    <path
+                      fill="#eb4335"
+                      d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"
+                    />
+                  </svg>
+                  Login with Google
+                </Button>
+              </div>
+              <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                <span className="bg-card text-muted-foreground relative z-10 px-2">
+                  atau
+                </span>
+              </div>
+              <div className="grid gap-2">
+                <p className="text-xs text-center">
+                  Masuk sebagai Staf/Pemerintah
+                </p>
+                <Button
+                  type="submit"
+                  className="w-full rounded-full bg-blue-500 hover:bg-blue-600"
+                >
+                  Masuk
+                </Button>
+
+                <div className="flex text-xs items-start gap-x-1 text-slate-500 p-1 rounded border border-gray-300">
+                    <WarningCircleIcon size={20} />
+                    <p>
+                      Untuk pihak terkait yang terlibat dalam pengelolaan dan
+                      pemantauan layanan
+                    </p>
+
+                </div>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        Dengan menekan lanjut, berarti anda menyetujui <a href="#">Terms of Service</a>{" "}
+        dan <a href="#">Privacy Policy</a> kami.
+      </div>
+    </div>
+  );
+}
